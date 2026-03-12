d0f885f2b5638019153fe36d83bb0f536899c304
GeekCoder403 <gauthierc2@cssdd.ca>
2026-03-12 15:13:45 -0400

    dsadasdsadsadsadasdasdasdasdasd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const dburl = 'ohsyvubwgqnlwesxecnb';
+const dburl = 'https://ohsyvubwgqnlwesxecnb.supabase.co';
 const dbkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oc3l2dWJ3Z3FubHdlc3hlY25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDIwODgsImV4cCI6MjA4ODkxODA4OH0.wQaXZRmoCRThU4rlcTPJjdPewiYv63K0vwYQvi91h4M';
 const db = supabase.createClient(dburl, dbkey);
 
