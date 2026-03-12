e873cfc2b2c6ce2f5aae8538ca0b2e643b14b312
GeekCoder403 <gauthierc2@cssdd.ca>
2026-03-12 15:11:12 -0400

    fddsafdsfdsfdsfds

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
 const dburl = 'ohsyvubwgqnlwesxecnb';
 const dbkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oc3l2dWJ3Z3FubHdlc3hlY25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDIwODgsImV4cCI6MjA4ODkxODA4OH0.wQaXZRmoCRThU4rlcTPJjdPewiYv63K0vwYQvi91h4M';
 const db = createClient(dburl, dbkey);
 
-
+async function d() {
+    const { data, error } = await db.from('database').select('*');
+    console.log(data)
+}
